c6173fa26121a07b87a64a21861b74577f78ffb9
Billy Schnetzler <bschnetzler2739@mail.kvcc.edu>
2023-04-10 16:34:44 -0400

    Install adminlte2-pdq. Create requirements.txt Add adminlte-2 middleware to settings.

--- django_project/settings.py
+++ django_project/settings.py
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'adminlte2_pdq.middleware.AuthMiddleware'
 ]
 
 ROOT_URLCONF = 'django_project.urls'
