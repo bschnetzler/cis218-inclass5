fc63cf26e0d14ed6636e601f077a4c1c4761a9bf
Billy Schnetzler <bschnetzler2739@mail.kvcc.edu>
2023-04-22 14:47:17 -0400

    Set up for deployment.

--- django_project/settings.py
+++ django_project/settings.py
@@ -26,9 +26,13 @@ SECRET_KEY = 'django-insecure-@uyft!jk!ok!**8ytiuf&vb-z5x(k%^=#^u@#fdru70b!nlmuv
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '.herokuapp.com',
+    'localhost',
+    '127.0.0.1',
+]
 
 
 # Application definition
